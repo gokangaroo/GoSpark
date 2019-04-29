a25923ea99a903efcc69a10b285ff0f7564dce71
GeekGhc <ghcgavin@sina.com>
2019-04-29 23:44:56 +0800

    GoSpark

--- main.go
+++ main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"net/http"
-	"log"
-	"fmt"
-	)
+	_ "GoSpark/routers"
+	"github.com/astaxie/beego"
+)
 
-func home(w http.ResponseWriter,r *http.Request)  {
-	r.ParseForm()  // 解析参数，默认是不会解析的
-	fmt.Fprintf(w, "Hello GoSpark!") // 这个写入到 w 的是输出到客户端的
-}
-func main()  {
-	http.HandleFunc("/",home)// 设置访问的路由
-	err := http.ListenAndServe(":9000",nil)// 设置监听的端口
-	if err != nil{
-		log.Fatal("ListenAndServer: ",err)
-	}
+func main() {
+	beego.Run()
 }
 
