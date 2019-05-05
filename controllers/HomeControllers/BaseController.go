440bdcb0054e8c30ed4d77c1cfd6f66e98525308
huija <1150555483@qq.com>
2019-05-05 21:12:14 +0800

    i18n

--- controllers/HomeControllers/BaseController.go
+++ controllers/HomeControllers/BaseController.go
@@ -1,10 +1,39 @@
 package HomeControllers
 
-import "github.com/astaxie/beego"
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/beego/i18n"
+)
 
 type BaseController struct{
 	beego.Controller
 	TplTheme string //模板主题
 	TplStatic string //静态文件
 	IsLogin int // 用户是否登录
+	i18n.Locale // 国际化
+}
+
+func (this *BaseController) Prepare() {
+	// Reset language option.
+	this.Lang = "" // This field is from i18n.Locale.
+
+	// 1. Get language information from 'Accept-Language'.
+	// 这个根据用户浏览器的语言设置走.
+	al := this.Ctx.Request.Header.Get("Accept-Language")
+	if len(al) > 4 {
+		al = al[:5] // Only compare first 5 letters.
+		logs.Trace("Browser's setup of language is : "+al)
+		if i18n.IsExist(al) {
+			this.Lang = al
+		}
+	}
+
+	// 2. Default language is America English.
+	if len(this.Lang) == 0 {
+		this.Lang = "en-US"
+	}
+
+	// 3.模板中显示语言，不通过控制器
+	//this.Data["Lang"] = this.Lang
 }
--- controllers/default.go
+++ controllers/default.go
@@ -1,15 +1,39 @@
 package controllers
 
 import (
+	. "GoSpark/controllers/HomeControllers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/beego/i18n"
+	"strings"
 )
 
+var LangTypes []string // Languages that are supported.
+func init() {
+	// Initialize language type list.
+	LangTypes = strings.Split(beego.AppConfig.String("lang_types"), "|")
+
+	// Load locale files according to language types.
+	for _, lang := range LangTypes {
+		logs.Trace("Loading language: " + lang)
+		if err := i18n.SetMessage(lang, "conf/"+"locale_"+lang+".ini"); err != nil {
+			logs.Error("Fail to set message file:", err)
+			return
+		}
+	}
+}
+
 type MainController struct {
-	beego.Controller
+	BaseController
 }
 
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
+	//中英文转换
+	c.Data["welcome"] = c.Tr("welcome")
+	c.Data["descripition"] = c.Tr("descripition")
+	c.Data["site"] = c.Tr("site")
+	c.Data["contact"] = c.Tr("contact")
 }
