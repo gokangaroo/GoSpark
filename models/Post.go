ff4eb89949b40a379bdeea44ff6049959741af2d
GeekGhc <ghcgavin@sina.com>
2019-05-22 00:16:27 +0800

    用户动态表

--- models/Post.go
+++ models/Post.go
@@ -7,7 +7,7 @@ type Post struct {
 	Id    int    `orm:"column(id);auto"`
 	Title string `orm:"size(16);unique;column(title)"` //标题
 	User  *User  `orm:"rel(fk)"`    //设置一对多关系
-	Tags  []*Tag `orm:"rel(m2m)"`   //设置多对多
+	Tags  []*Tag `orm:"rel(m2m);rel_table(post_tag)"`   //设置多对多
 }
 
 func (t *Post) TableName() string {
--- models/User.go
+++ models/User.go
@@ -1,16 +1,23 @@
 package models
 
-import "github.com/astaxie/beego/orm"
+import (
+	"github.com/astaxie/beego/orm"
+	"time"
+)
 
 //用户表
 type User struct {
-	Id       int      `orm:"column(id);auto"`
-	Email    string   `orm:"size(50);unique;column(email);default();"` //邮箱
-	Password string   `orm:"size(32);column(password)"`                //密码
-	Username string   `orm:"size(16);unique;column(username)"`         //用户名
-	Intro    string   `orm:"size(255);default();column(intro)"`        //个性签名
-	Post     []*Post  `orm:"reverse(many)"` //一对多的反向关系 (用户-文章)
-	Profile  *Profile `orm:"rel(one)"`      // 一对一(用户-概况)
+	Id       int       `orm:"column(id);auto"`
+	Email    string    `orm:"size(50);unique;column(email);default();"`       //邮箱
+	Avatar   string    `orm:"size(255);unique;column(avatar);default();"`     //头像
+	Password string    `orm:"size(32);column(password)"`                      //密码
+	Username string    `orm:"size(16);unique;column(username)"`               //用户名
+	Intro    string    `orm:"size(255);default();column(intro)"`              //个性签名
+	Post     []*Post   `orm:"reverse(many);on_delete(set_null)"`              //一对多的反向关系 (用户-文章)
+	Profile  *Profile  `orm:"rel(one);on_delete(set_null)"`                   // 一对一(用户-概况)
+	IsActive int       `orm:"column(is_active);default(0)"`                   //是否激活
+	Created  time.Time `orm:"auto_now_add;column(created_at);type(datetime)"` //创建时间
+	Updated  time.Time `orm:"auto_now;column(updated_at);type(datetime)"`     //更新时间
 }
 
 func getUser() *User {
