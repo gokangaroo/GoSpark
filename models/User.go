124f80c6dbe3f90126822e1383ca3f6fb1f197e0
GeekGhc <ghcgavin@sina.com>
2019-06-30 17:28:57 +0800

    登录注册页面

--- models/User.go
+++ models/User.go
@@ -8,11 +8,12 @@ import (
 //用户表
 type User struct {
 	Id       int       `orm:"column(id);auto"`
-	Username string    `orm:"column(username);size(255);unique"`              //用户名
+	Name     string    `orm:"column(name);size(32);unique"`               //用户名
+	Username string    `orm:"column(username);size(64);unique"`               //用户名
 	Email    string    `orm:"column(email);size(255);unique;default();"`      //邮箱
-	Phone    string    `orm:"column(phone);size(64);default()"`       //邮箱
+	Phone    string    `orm:"column(phone);size(64);default()"`               //邮箱
 	Avatar   string    `orm:"column(avatar);size(255);unique;default();"`     //头像
-	Password string    `orm:"column(password);size(255)"`                     //密码
+	Password string    `orm:"column(password);size(64)"`                      //密码
 	Intro    string    `orm:"column(intro);size(255);default()"`              //个性签名
 	Post     []*Post   `orm:"reverse(many);on_delete(set_null)"`              //一对多的反向关系 (用户-文章)
 	Profile  *Profile  `orm:"rel(one);"`                                      // 一对一(用户-概况)
